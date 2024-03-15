002d3040dded336079517fa53a96fbf04c0c395c
adamd <adam.d.hinton@gmail.com>
2024-03-15 10:37:55 -0400

    initialize list of names to work on

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,15 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package dev.lpa;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        String[] names = {"Jimbo", "BeepBeep", "anna", "bob", "hillbilly", "Squilliam", "DrBtthl", "Zack", "Sissy",
+                "mindy"};
     }
 }
+
+
+// Make String[] with first names in mixed case
+    // At least one palindrome name (Bob, Anna)
+// Use Arrays.setAll or List.replaceAll to change vals in array
+// If you use List methods you'll need a list backed by the array, so that changes get made to the initial array
+    // In other words, don't make copy of the array
+
+// Do the following using one of the above two methods, with appropriate lambdas:
+    // Transofmr names to all uppercase
+    // Add randomly generated middle initial, including period
+    // Add last name that is reverse of first name
+    // Print array or its elements, using forEach, at least once
+    // create a new modifiable Arraylist from this array, removing any names where the last name equals the first name.
+        // Use removeIf with lambda
+
+// NOTES:
+// Can make helper methods
